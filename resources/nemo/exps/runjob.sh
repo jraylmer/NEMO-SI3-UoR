d294e0a48d7b0c1cf38bb4a25a7bb1bdca76c085
jraylmer <j.aylmer@reading.ac.uk>
2025-12-08 10:49:38 +0000

    Update compiler options and resources for XIOS and NEMO
    * Clean up the compiler options files and flag settings for NEMO and XIOS build scripts
    * Add environment configuration file for NEMO to eliminate need for explicit module loading
    * Update template job submission script to resolve some issues where jobs hang when running on RACC2

--- resources/nemo/exps/runjob.sh
+++ resources/nemo/exps/runjob.sh
@@ -4,14 +4,16 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --threads-per-core=1
 #SBATCH --nodes=1-1
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --job-name=EXP01
 #SBATCH --output=%x-%j.out
 #SBATCH --time=00:30:00
 
+# Prepare environment:
+module purge
 module load mpi/openmpi-x86_64
 
-# Do not use srun: the version of OpenMPI on RACC2 does not support it and
-# mpiexec is recommended by OpenMPI anyway:
-mpiexec ./nemo
+export OMPI_MCA_fs_ufs_lock_algorithm=1
+export HDF5_USE_FILE_LOCKING=FALSE
 
+mpirun ./nemo
